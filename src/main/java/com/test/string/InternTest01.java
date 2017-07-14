de7035f9b0f166e5af7f61d18c18b8981bd616e7
belong <belong.belong@outlook.com>
2017-07-14 17:49:02 +0800

    添加一些测试类

---
+++ src/main/java/com/test/string/InternTest01.java
@@ -0,0 +1,15 @@
+package com.test.string;
+
+/**
+ * @Description: <p></p>
+ * @Author: belong.
+ * @Date: 2017/7/14.
+ */
+public class InternTest01 {
+    public static void main(String[] args) {
+        String str1 = "str01";
+        String str2 = new String("str")+new String("01");
+        str2.intern();
+        System.out.println(str2 == str1);//#8
+    }
+}
